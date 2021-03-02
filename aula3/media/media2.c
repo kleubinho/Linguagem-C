daf4d026d41ad69e3e03ec74872a0baadc672148
kleubinho <79463086+kleubinho@users.noreply.github.com>
2021-03-02 20:12:41 -0300

    Aula 3
    aula 3

---
+++ aula3/media/media2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/* 
+	Cálculo da média
+	@author Kleber Henrique
+*/
+
+int main(int argc, char *argv[]) {
+	setlocale(LC_ALL,"Portuguese");
+	printf("Cálculo da média\n");
+	//variáveis
+	float nota1,nota2,media;
+	//entrada
+	printf("Digite a nota 1: ");
+	scanf("%f", &nota1);
+	printf("DIgite a nota 2: ");
+	scanf("%f", &nota2);
+	//processamento
+	media = ( nota1 + nota2) / 2;
+	//saída
+	printf("Resultado: %.1f\n", media);
+	system("pause");
+	return 0;
+}
